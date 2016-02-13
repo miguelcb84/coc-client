68682b34b650c36bcf3045fcb3d0820397f991c4
Miguel Coronado <miguelcb84@gmail.com>
2016-02-13 16:46:07 +0100

    Improve documentation and add manifest

--- coc/__init__.py
+++ coc/__init__.py
@@ -5,6 +5,7 @@ Clash of clans API client
 from textwrap import dedent
 
 from .api import ClashOfClans, ApiCall, ApiResponse
+from .utils import filter_country_locations
 
 
 __doc__ = __doc__ or ""
@@ -17,6 +18,7 @@ __doc__ += dedent(ClashOfClans.__doc__ or "")
 
 
 
-___all__= ["ClashOfClans", 
+___all__= ["filter_country_locations",
+           "ClashOfClans", 
            "ApiCall", 
            "ApiResponse"]
--- coc/api.py
+++ coc/api.py
@@ -5,14 +5,19 @@ import urllib
 
 
 def build_uri(endpoint, api_version, uri_parts, uri_args={}):
-    ''' Build the URL using the endpoint, the api version, the uri parts and the args.
+    """ Build the URL using the endpoint, the api version, the uri parts and the args.
     
-    The resulting uri is as follows:
+        :param dict uri_args: parameters to include in the URL. 
+        :param tuple uri_parts: url encoded and `uri_parts` too.
+        :return: A string that represents the absolute URL of the request
+        :rtype : str
     
-    {endpoint}/{api_version}/{uri_part1}/.../{uri_partn}?{uri_args}
+        The resulting uri is as follows:
     
-    `uri_args` are url encoded and `uri_parts` too.
-     '''
+        {endpoint}/{api_version}/{uri_part1}/.../{uri_partn}?{uri_args}
+        
+        The `uri_args` and the `uri_parts` are url encoded.
+    """
     # to unicode
     uri_parts = [unicode(x) for x in uri_parts]
     # and encoded 
@@ -24,13 +29,17 @@ def build_uri(endpoint, api_version, uri_parts, uri_args={}):
     # add params if any
     if uri_args:
         url_to_call = "{}?{}".format(url_to_call, urllib.urlencode(uri_args))
-    # log
-    print ("URI BUILT:{}".format(url_to_call))
+    # return
     return url_to_call
 
 
 def wrap_response(resp):
-    """ """
+    """ Wrap the requests response in an `ApiResponse` object 
+    
+        :param object resp: response object provided by the `requests` library.
+        :return: An `ApiResponse` object that wraps the content of the response.
+        :rtype: object, list or dict
+    """
     try:
         js_resp = resp.json()
         if resp.ok:
@@ -65,53 +74,53 @@ class ApiDictResponse(dict, ApiResponse):
     
 
 class ApiCall(object):
-    ''' 
-    Minified REST API call generator using attributes.
+    """ Minified REST API call generator using attributes.
     
-    REST URLs are replicated using properties and method calls as shown. 
-    The URI path:
+        REST URLs are replicated using properties and method calls as shown. 
+        The URI path:
+            
+            /locations/12003/rank/global
         
-        /locations/12003/rank/global
-    
-    Is replicated using sequence:
+        Is replicated using sequence:
+            
+            api.locations(12003).rank.global
+            
+        In order make the api call, the methods `get` and `post` are provided 
+        (each using a different http method). In the call, the endpoint and the 
+        version of the api are included as the URL base. So, a sequence:
         
-        api.locations(12003).rank.global
+            api=ApiCall(endpoint='http://example.org', api_version='v1')
+            api.locations(12003).rank.global.get()
+            
+        Will perform an http get to the URL:
         
-    In order make the api call, the methods `get` and `post` are provided 
-    (each using a different http method). In the call, the endpoint and the 
-    version of the api are included as the URL base. So, a sequence:
-    
-        api=ApiCall(endpoint='http://example.org', api_version='v1')
-        api.locations(12003).rank.global.get()
+            http://example.org/v1/locations/12003/rank/global
+            
+        The way it works, it that each access to `ApiCall` attributes returns a 
+        new API call with the attributtes with the existing one, but it appends
+        the uri part. Sequential call, thus produce the explained result.
         
-    Will perform an http get to the URL:
-    
-        http://example.org/v1/locations/12003/rank/global
+        Note that attributes starting with an underscore are omitted.
         
-    The way it works, it that each access to `ApiCall` attributes returns a 
-    new API call with the attributtes with the existing one, but it appends
-    the uri part. Sequential call, thus produce the explained result.
-    
-    Note that attributes starting with an underscore are omitted.
-    
-    Additionally, arguments are supported. To use them you must provide dictionary like parameters:
-    
-        api.clans(name='theclan', minMembers=10)
-    
-    Args:
-        `bearer_token` - the API key provided 
-        `endpoint` - The endpoint od the API
-        `api_version` - The version of the API. Used to build the API url
-        `extract_items` - Boolean. If true, response will be parsed and wraped 
-            in a list or dictionary. Otherwise, the requests response will be 
-            returned. 
-        `uri_parts` - tuple. Provide an initial value to uri_parts. Used with 
-            recursive calls by the `__getattr__` and `__call__` methods. 
-        `uri_args` - arguments of the call.
-    '''
+        Additionally, arguments are supported. To use them you must provide dictionary like parameters:
+        
+            api.clans(name='theclan', minMembers=10)
+    """
     
     def __init__(self, bearer_token, endpoint, api_version, 
                  extract_items=True, uri_parts=None, uri_args={}):
+        """ Construct an ApiCall object.
+        
+            :param str bearer_token: the API key provided 
+            :param str endpoint: The endpoint od the API
+            :param str api_version: The version of the API. Used to build the API url
+            :param boolean extract_items: If true, response will be parsed and wraped 
+                in a list or dictionary. Otherwise, the requests response will be 
+                returned. 
+            :param tuple uri_parts: Provide an initial value to uri_parts. Used with 
+                recursive calls by the `__getattr__` and `__call__` methods. 
+            :param dict uri_args: arguments of the call. 
+        """
         self.bearer_token = bearer_token
         self.endpoint = endpoint
         self.api_version = api_version
@@ -123,9 +132,10 @@ class ApiCall(object):
             self.uri_parts = uri_parts
     
     def __getattr__(self, k):
-        ''' Append the name of the attribute to the uri_parts tuple. 
-        Attributes starting with an underscore are omitted. `self` is returned 
-        to enable chainability. '''
+        """ Append the name of the attribute to the uri_parts tuple. 
+            Attributes starting with an underscore are omitted. `self` is returned 
+            to enable chainability. 
+        """
         if k.startswith("_"):
             pass
         else:
@@ -135,10 +145,9 @@ class ApiCall(object):
                            uri_parts=self.uri_parts + (k,), uri_args=self.uri_args)
                     
     def __call__(self, *args, **kwargs):
-        ''' Append the arguments to the `uri_parts` tuple. `self` is returned 
-        to enable chainability. '''
-        print('CALL:{}'.format(args))
-        print('KWARGS:{}'.format(kwargs))
+        """ Append the arguments to the `uri_parts` tuple. `self` is returned 
+            to enable chainability. 
+        """
         if args or kwargs:
             uri_args = {}
             if kwargs:
@@ -150,6 +159,7 @@ class ApiCall(object):
         return self
 
     def build_headers(self):
+        """ Build the required headers to make the API call """
         return {"Accept": "application/json", "authorization": "Bearer {}".format(self.bearer_token)}
 
     def _process_call(self, method):
@@ -171,64 +181,63 @@ class ApiCall(object):
 
     
 class ClashOfClans(ApiCall):
-    """ 
-    Create a new Clash of clans connector.
+    """ Create a new Clash of clans connector.
     
-    Use the `bearer_token` to identify your call.
-    
-    `endpoint` lets you change the api endpoint which is used to build the base URI. 
-     By default is uses 'https://api.clashofclans.com'.
-    
-    `api_version` is used to build the base URI. By default it uses 'v1'.
-    
-    Examples:
-    
-        To start the client:
+        Use the `bearer_token` to identify your call.
+        
+        `endpoint` lets you change the api endpoint which is used to build the base URI. 
+         By default is uses 'https://api.clashofclans.com'.
+        
+        `api_version` is used to build the base URI. By default it uses 'v1'.
+        
+        Examples:
+        
+            To start the client:
+                
+                from coc import *
+                coc = ClashOfClans(bearer_token=<api_key>)
+        
+            To access all locations use the call (GET /locations)
             
-            from coc import *
-            coc = ClashOfClans(bearer_token=<api_key>)
-    
-        To access all locations use the call (GET /locations)
-        
-            coc.locations.get()
-        
-        To access a particular location given by an id (GET /locations/{locationId})
-        
-            coc.locations(32000218).get()
-        
-        To access the rankings of a location GET /locations/{locationId}/rankings/{rankingId}
-        
-            coc.locations(32000218).rankings('clans').get()
-            coc.locations(32000218).rankings.clans.get()
-        
-        Note that attributes starting with an underscore are omitted.
-        
-        To include parameters in the call use provide dictionary like arguments to the call:
-    
-            coc.clans(name='theclan', minMembers=10).get()
-        
-        This produces /clans?name=theclan&minMembers=10. The parameters are uri encoded.
+                coc.locations.get()
             
-    
-    Args:
-        `bearer_token` - the AP key provided by CoC 
-        `endpoint` - the endpoint of the API. Default value is https://api.clashofclans.com 
-        `api_version` - the version of the API. Default value is v1
-        `extract_items` - if True, the response will be parsed and wraped in a list or 
-            dictionary. Otherwise, the requests response will be returned.
+            To access a particular location given by an id (GET /locations/{locationId})
+            
+                coc.locations(32000218).get()
+            
+            To access the rankings of a location GET /locations/{locationId}/rankings/{rankingId}
+            
+                coc.locations(32000218).rankings('clans').get()
+                coc.locations(32000218).rankings.clans.get()
+            
+            Note that attributes starting with an underscore are omitted.
+            
+            To include parameters in the call use provide dictionary like arguments to the call:
+        
+                coc.clans(name='theclan', minMembers=10).get()
+            
+            This produces /clans?name=theclan&minMembers=10. The parameters are uri encoded.
+        
     """
-    
     def __init__(self, 
                  bearer_token,
                  endpoint='https://api.clashofclans.com', 
                  api_version='v1',
                  extract_items=True):
+        """ Contruct a ClashOfClans client.
         
+            :param str bearer_token: the AP key provided by CoC 
+            :param str endpoint: the endpoint of the API. Default value is https://api.clashofclans.com 
+            :param str api_version: the version of the API. Default value is v1
+            :param boolean extract_items: if True, the response will be parsed and wraped in a list or 
+                    dictionary. Otherwise, the requests response will be returned.
+        """
         super(ClashOfClans, self).__init__(
             bearer_token=bearer_token,
             endpoint=endpoint,
             api_version=api_version,
             extract_items=extract_items, 
             uri_parts=None)
+    
     
 ___all__= ["ClashOfClans", "ApiCall", "ApiResponse"]
