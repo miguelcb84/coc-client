208f4a43bce8d58804d9c202c9897e64189f23c1
Miguel Coronado <miguelcb84@gmail.com>
2016-02-13 17:52:18 +0100

    Change version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'coc',
   packages = ['coc'],
-  version = '0.1',
+  version = '0.2',
   description = 'A simple yet fully featured ClashOfClans API client',
   author = 'Miguel Coronado',
   author_email = 'miguelcb84@gmail.com',
   url = 'https://github.com/miguelcb84/coc-client',
-  download_url = 'https://github.com/miguelcb84/coc-client/tarball/0.1',                  
+  download_url = 'https://github.com/miguelcb84/coc-client/tarball/0.2',                  
   keywords = ['coc', 'clash of clans', 'api client'], 
   classifiers = [],
 )
